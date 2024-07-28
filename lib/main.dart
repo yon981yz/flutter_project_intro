ae1742ecfa1dbf5bf237aa9a8998269125ee5fad
LrZl <dingtti@naver.com>
2024-07-28 16:16:33 +0900

    sw
    sw

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project_intro/view/changbin.dart';
-import 'package:flutter_project_intro/view/home.dart';
+import 'package:flutter_project_intro/view/sw.dart';
+import 'package:flutter_project_intro/view/yonghyuk.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,16 +14,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       routes: {
-        '/' : (context) => const home(),
-        '/cb' : (context) => const Changbin(),
-        // '/ws' : (context) => const Changbin(),
-        // '/sy' : (context) => const Changbin(),
-        // '/yh' : (context) => const Changbin(),
+        '/' : (context) => const Sw(),
       },
       initialRoute: '/',
     );
