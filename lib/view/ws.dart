876c794c23a1ae1b04c536c41f9524cf86c8f0d3
ppochacco <wsjeong0608@gmail.com>
2024-07-28 23:14:17 +0900

    body에 pop추가
    화면 터치하면 뒤로가요

--- lib/view/ws.dart
+++ lib/view/ws.dart
@@ -12,126 +12,114 @@ class Introducews extends StatelessWidget {
         foregroundColor: Colors.white,
         backgroundColor: const Color.fromARGB(137, 81, 183, 242),
       ),
-              body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('images/backgroundimg.png'), // 이미지 경로
-              fit: BoxFit.cover,
-              ),
-              ),
-              child: const Row(mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(50.0),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.all(15.0),
-                          child: Text('Love',
-                          style: TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.bold
+              body: GestureDetector(onTap: () => Navigator.pop(context),
+                child: Container(
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                image: AssetImage('images/backgroundimg.png'), // 이미지 경로
+                fit: BoxFit.cover,
+                ),
+                ),
+                child: const Row(mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(50.0),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(15.0),
+                            child: Text('Love',
+                            style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold
+                            ),
+                            ),
                           ),
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text('디저트', style: TextStyle(
+                              fontSize: 20, color: Colors.white
+                            ),),
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Text('디저트', style: TextStyle(
-                            fontSize: 20, color: Colors.white
-                          ),),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Text('여행', style: TextStyle(
-                            fontSize: 20, color: Colors.white
-                          ),),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Text('바다', style: TextStyle(
-                            fontSize: 20, color: Colors.white
-                          ),),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Text('새로운 것', style: TextStyle(
-                            fontSize: 20, color: Colors.white
-                          ),),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Text('(털 있는)동물', style: TextStyle(
-                            fontSize: 20, color: Colors.white
-                          ),),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Text('PIXAR 영화', style: TextStyle(
-                            fontSize: 20, color: Colors.white
-                          ),),
-                        ),
-                      ],
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text('여행', style: TextStyle(
+                              fontSize: 20, color: Colors.white
+                            ),),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text('바다', style: TextStyle(
+                              fontSize: 20, color: Colors.white
+                            ),),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text('(털 있는)동물', style: TextStyle(
+                              fontSize: 20, color: Colors.white
+                            ),),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text('PIXAR 영화', style: TextStyle(
+                              fontSize: 20, color: Colors.white
+                            ),),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(50.0),
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.all(15.0),
-                          child: Text('Hate',
-                          style: TextStyle(
-                            fontSize: 30, fontWeight: FontWeight.bold
+                    Padding(
+                      padding: EdgeInsets.all(50.0),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(15.0),
+                            child: Text('Hate',
+                            style: TextStyle(
+                              fontSize: 30, fontWeight: FontWeight.bold
+                            ),
+                            ),
                           ),
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text('해산물', style: TextStyle(
+                              fontSize: 20, color: Colors.black38
+                            ),
+                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Text('해산물', style: TextStyle(
-                            fontSize: 20, color: Colors.black38
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text('시끄러운 장소', style: TextStyle(
+                              fontSize: 20, color: Colors.black38
+                            ),
+                            ),
                           ),
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text('산', style: TextStyle(
+                              fontSize: 20, color: Colors.black38
+                            ),
+                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Text('시끄러운 장소', style: TextStyle(
-                            fontSize: 20, color: Colors.black38
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text('벌레', style: TextStyle(
+                              fontSize: 20, color: Colors.black38
+                            ),
+                            ),
                           ),
+                          Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: Text('종교 권유', style: TextStyle(
+                              fontSize: 20, color: Colors.black38
+                            ),
+                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Text('산', style: TextStyle(
-                            fontSize: 20, color: Colors.black38
-                          ),
-                          ),
-                        ),
-                    
-                        Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Text('지루한 것', style: TextStyle(
-                            fontSize: 20, color: Colors.black38
-                          ),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Text('벌레', style: TextStyle(
-                            fontSize: 20, color: Colors.black38
-                          ),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Text('종교 권유', style: TextStyle(
-                            fontSize: 20, color: Colors.black38
-                          ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                ),
               ),
               drawer: Drawer(
         child: ListView(
